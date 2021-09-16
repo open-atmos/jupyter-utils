e1db19153d5d69c80867e5cc4f5bec670ac88ec7
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-09-16 14:34:18 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import os
+
+
+def get_long_description():
+    with open("README.md", "r", encoding="utf8") as file:
+        long_description = file.read()
+    return long_description
+
+
+setup(
+    name='atmos-cloud-sim-uj-utils',
+    description='utility routines used in PySDM and PyMPDATA examples and tests',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    install_requires=[],
+    author='https://github.com/atmos-cloud-sim-uj/utils/graphs/contributors',
+    author_email='sylwester.arabas@uj.edu.pl',
+    license="GPL-3.0",
+    packages=find_packages(include=['atmos_cloud_sim_uj_utils', 'atmos_clous_sim_uj_utils.*']),
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown"
+)
