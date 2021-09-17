8ef9efe746beabdf4bc00ff0da47fa3907372968
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-09-17 11:41:29 -0500

    satisfying pylint :)

--- atmos_cloud_sim_uj_utils/pip_install_on_colab.py
+++ atmos_cloud_sim_uj_utils/pip_install_on_colab.py
@@ -1,8 +1,14 @@
+# pylint: disable-next=missing-module-docstring
+
 def pip_install_on_colab(*args):
+    """executes pip (and ldconfig) ensuring Colab subtleties are handled as sane as it can get"""
+    # pylint: disable-next=import-outside-toplevel
+    # pylint: disable-next=multiple-imports
     import subprocess, notebook, tornado, sys
-    subprocess.check_call([sys.executable, '-m', 'pip', '--quiet', '--use-deprecated=legacy-resolver', 'install',
-                           f'notebook=={notebook.__version__}',
-                           f'tornado=={tornado.version}',
-                           *args]
-                          )
+    subprocess.check_call([
+      sys.executable, '-m', 'pip', '--quiet', '--use-deprecated=legacy-resolver', 'install',
+      f'notebook=={notebook.__version__}',
+      f'tornado=={tornado.version}',
+      *args
+    ])
     subprocess.check_call(['ldconfig'])
