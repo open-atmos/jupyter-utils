6c69eebefd1649f58d96face7e99f7f147d699c2
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-09-19 11:13:10 -0500

    Update __init__.py

--- atmos_cloud_sim_uj_utils/__init__.py
+++ atmos_cloud_sim_uj_utils/__init__.py
@@ -1,5 +1,5 @@
 """
-utility routines used in PySDM and PyMPDATA examples and tests
+utility routines used in PySDM and PyMPDATA example Jupyter notebooks
 """
 from atmos_cloud_sim_uj_utils.pip_install_on_colab import pip_install_on_colab
 from atmos_cloud_sim_uj_utils.temporary_file import TemporaryFile
