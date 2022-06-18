f3e18a8c068bdfe4870eb0e21fe6b7b404079f66
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-06-18 06:53:59 -0500

    adding PyPI release automation

--- setup.py
+++ setup.py
@@ -14,7 +14,10 @@ def get_long_description():
 setup(
     name='atmos-cloud-sim-uj-utils',
     description='utility routines used in PySDM and PyMPDATA examples and tests',
-    use_scm_version=True,
+    use_scm_version={
+        "local_scheme": lambda _: "",
+        "version_scheme": "post-release"
+    },
     setup_requires=['setuptools_scm'],
     install_requires=['ipywidgets', 'IPython', 'matplotlib'],
     author='https://github.com/atmos-cloud-sim-uj/utils/graphs/contributors',
