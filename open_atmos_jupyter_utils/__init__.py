6ba81414593e41fe1ad77dd6a52bb4d31f5f4d78
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2024-05-31 00:02:18 +0200

    render README contents in docstring-based API docs (#20)

--- open_atmos_jupyter_utils/__init__.py
+++ open_atmos_jupyter_utils/__init__.py
@@ -1,5 +1,5 @@
 """
-utility routines used in PySDM and PyMPDATA example Jupyter notebooks
+.. include::../README.md
 """
 from open_atmos_jupyter_utils.pip_install_on_colab import pip_install_on_colab
 from open_atmos_jupyter_utils.temporary_file import TemporaryFile
