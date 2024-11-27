f23c97d02b23906065e7f835fb4e925d3dd9c631
sfonxu <wronski.michal4@wp.pl>
2024-11-27 15:45:57 +0100

    fixes to pyproject.toml to help with compiling

--- setup.py
+++ setup.py
@@ -12,17 +12,12 @@ def get_long_description():
 
 
 setup(
-    name='open-atmos-jupyter-utils',
-    description='utility routines used in PySDM, PyMPDATA and PyPartMC examples and tests',
     use_scm_version={
         "local_scheme": lambda _: "",
         "version_scheme": "post-release"
     },
     setup_requires=['setuptools_scm'],
     install_requires=['ipywidgets', 'IPython', 'matplotlib', 'imageio'],
-    author='https://github.com/open-atmos/jupyter-utils/graphs/contributors',
-    author_email='sylwester.arabas@agh.edu.pl',
-    license="GPL-3.0",
     packages=find_packages(include=['open_atmos_jupyter_utils', 'open_atmos_jupyter_utils.*']),
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
