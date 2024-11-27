177e375267c353ad83eee513dc90ecb7cbd6cb61
sfonxu <wronski.michal4@wp.pl>
2024-11-27 15:51:56 +0100

    fixes to pyproject.toml to help with compiling

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         "version_scheme": "post-release"
     },
     setup_requires=['setuptools_scm'],
-    install_requires=['ipywidgets', 'IPython', 'matplotlib', 'imageio'],
+    #install_requires=['ipywidgets', 'IPython', 'matplotlib', 'imageio'],
     packages=find_packages(include=['open_atmos_jupyter_utils', 'open_atmos_jupyter_utils.*']),
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
