93553800c59151132822a7084c53754b2f25448b
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2024-11-29 00:29:56 +0100

    removing double metadata from setup.py

--- setup.py
+++ setup.py
@@ -14,8 +14,6 @@ def get_long_description():
 
 setup(
     use_scm_version={"local_scheme": lambda _: "", "version_scheme": "post-release"},
-    setup_requires=["setuptools_scm", "setuptools"],
-    install_requires=["ipywidgets", "IPython", "matplotlib", "imageio"],
     packages=find_packages(
         include=["open_atmos_jupyter_utils", "open_atmos_jupyter_utils.*"]
     ),
