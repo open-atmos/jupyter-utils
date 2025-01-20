11afb9c2ecf031b7e603376cbd295d77a11352a3
Agnieszka Żaba <azaba@agh.edu.pl>
2025-01-20 17:37:35 +0100

    add notebook_bars to init file

--- open_atmos_jupyter_utils/__init__.py
+++ open_atmos_jupyter_utils/__init__.py
@@ -4,3 +4,4 @@
 from open_atmos_jupyter_utils.pip_install_on_colab import pip_install_on_colab
 from open_atmos_jupyter_utils.temporary_file import TemporaryFile
 from open_atmos_jupyter_utils.show_plot import show_plot, save_and_make_link
+from open_atmos_jupyter_utils.notebook_vars import notebook_vars
