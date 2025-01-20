f8d6bfa719a04b255e3800a2edfaef6dfa38ed26
Agnieszka Żaba <azaba@agh.edu.pl>
2025-01-20 18:59:44 +0100

    create test for notebook vars

---
+++ tests/test_notebook_vars.py
@@ -0,0 +1,21 @@
+"""
+test checking notebook_vars function
+"""
+
+from pathlib import Path
+import pytest
+
+from open_atmos_jupyter_utils import notebook_vars
+import examples
+
+@pytest.fixture(scope="session", name="notebook_variables")
+def notebook_variables_fixture():
+    """returns variables from the notebook """
+    print(examples.__file__)
+    return notebook_vars(
+        file=Path(examples.__file__).parent / "show_anim_example.ipynb",
+        plot=False,
+    )
+
+def test_notebook_vars(notebook_variables):
+    assert notebook_variables["frame_range"][-1]==49
