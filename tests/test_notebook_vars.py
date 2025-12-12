553a6829b41c7277fc34b181fa37c68599dda383
AgnieszkaZaba <56157996+AgnieszkaZaba@users.noreply.github.com>
2025-12-12 11:57:35 +0100

    close matplotlib figures in `notebook_vars`; enable testing notebook_vars; add plotting logic to notebook_vars test notebook (#66)

--- tests/test_notebook_vars.py
+++ tests/test_notebook_vars.py
@@ -4,18 +4,27 @@ test checking notebook_vars function
 
 from pathlib import Path
 import pytest
+import matplotlib
+
 
 from open_atmos_jupyter_utils import notebook_vars
-import examples
 
 @pytest.fixture(scope="session", name="notebook_variables")
 def notebook_variables_fixture():
     """returns variables from the notebook """
     return notebook_vars(
-        file=Path(examples.__file__).parent / "notebook_vars.ipynb",
+        file=str(Path(__file__).parent.parent / "examples" / "notebook_vars.ipynb"),
         plot=False,
     )
+class TestNotebookVars:
+    """test notebook_vars function"""
+    @staticmethod
+    def test_notebook_vars(notebook_variables):
+        """ checks for a value known only after notebook execution"""
+        assert notebook_variables["c"] == notebook_variables["a"] + notebook_variables["b"]
 
-def test_notebook_vars(notebook_variables):
-    """ checks for a value known only after notebook execution"""
-    assert notebook_variables["c"] == notebook_variables["a"] + notebook_variables["b"]
+    @staticmethod
+    def test_plots_closed(notebook_variables):
+        """ checks all figures closed """
+        assert isinstance(notebook_variables["fig"], matplotlib.figure.Figure)
+        assert 0 == len(matplotlib.pyplot.get_fignums())
