5c9dac2586a557780b14942ac498e871068d3d99
Agnieszka Żaba <azaba@agh.edu.pl>
2025-12-12 11:54:17 +0100

    revert fig removal

--- open_atmos_jupyter_utils/show_plot.py
+++ open_atmos_jupyter_utils/show_plot.py
@@ -8,16 +8,16 @@ from IPython.core.pylabtools import select_figure_formats
 from open_atmos_jupyter_utils.temporary_file import TemporaryFile
 
 
-def show_plot(filename=None, inline_format='svg'):
+def show_plot(filename=None, fig=pyplot, inline_format='svg'):
     """ the missing click-to-save-as-pdf-or-svg button for matplotlib/Jupyter 
     (use instead of *.show()) """
-    link = save_and_make_link(filename)
+    link = save_and_make_link(fig, filename)
     select_figure_formats(InteractiveShell.instance(), {inline_format})
     pyplot.show()
     display(link)
 
 
-def save_and_make_link(filename=None):
+def save_and_make_link(fig, filename=None):
     """ saves a figure as pdf and returns a Jupyter display()-able click-to-download widget """
     temporary_files = [
         TemporaryFile(suffix=suffix, filename=(
@@ -27,5 +27,5 @@ def save_and_make_link(filename=None):
         for suffix in ('.pdf', '.svg')
     ]
     for temporary_file in temporary_files:
-        pyplot.savefig(temporary_file.absolute_path, bbox_inches='tight')
+        fig.savefig(temporary_file.absolute_path, bbox_inches='tight')
     return HBox([temporary_file.make_link_widget() for temporary_file in temporary_files])
